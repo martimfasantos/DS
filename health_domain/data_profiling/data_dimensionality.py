e37d20bc83354d7504bf0937a64354543662f631
martimfasantos <marty.f.a.santos@gmail.com>
2022-11-27 01:59:42 +0000

    some bugs to solve and data distribution dataset2

--- health_domain/data_profiling/data_dimensionality.py
+++ health_domain/data_profiling/data_dimensionality.py
@@ -1,7 +1,7 @@
-from pandas import read_csv, DataFrame
+from pandas import read_csv
 from pandas.plotting import register_matplotlib_converters
 from matplotlib.pyplot import figure, savefig, show
-from ds_charts import bar_chart
+from ds_charts import bar_chart, get_variable_types
 
 register_matplotlib_converters()
 filename = '../datasets/classification/diabetic_data.csv'
@@ -9,71 +9,42 @@ data = read_csv(filename, na_values='?')
 
 # print(data.shape)
 
-def main():
-    # -------------------------------- #
-    # Nr of records vs nr of variables #
-    # -------------------------------- #
-    figure(figsize=(4,2))
-    values = {'nr records': data.shape[0], 'nr variables': data.shape[1]}
-    bar_chart(list(values.keys()), list(values.values()), title='Nr of records vs nr variables')
-    savefig('../images/records_variables.png')
-    show()
+# -------------------------------- #
+# Nr of records vs nr of variables #
+# -------------------------------- #
+figure(figsize=(4,2))
+values = {'nr records': data.shape[0], 'nr variables': data.shape[1]}
+bar_chart(list(values.keys()), list(values.values()), title='Nr of records vs nr variables')
+savefig('./images/records_variables.png')
+# show()
 
-    # -------------- #
-    # Variables Type #
-    # -------------- #
-    cat_vars = data.select_dtypes(include='object')
-    data[cat_vars.columns] = data.select_dtypes(['object']).apply(lambda x: x.astype('category'))
-    data.dtypes
+# -------------- #
+# Variables Type #
+# -------------- #
+cat_vars = data.select_dtypes(include='object')
+data[cat_vars.columns] = data.select_dtypes(['object']).apply(lambda x: x.astype('category'))
+data.dtypes
 
-    variable_types = get_variable_types(data)
-    counts = {}
-    for tp in variable_types.keys():
-        counts[tp] = len(variable_types[tp])
-    figure(figsize=(4,2))
-    bar_chart(list(counts.keys()), list(counts.values()), title='Nr of variables per type')
-    savefig('../images/variable_types.png')
-    show()
+variable_types = get_variable_types(data)
+counts = {}
+for tp in variable_types.keys():
+    counts[tp] = len(variable_types[tp])
+figure(figsize=(4,2))
+bar_chart(list(counts.keys()), list(counts.values()), title='Nr of variables per type')
+savefig('./images/variable_types.png')
+# show()
 
-    # -------------- #
-    # Missing values #
-    # -------------- #
-    mv = {}
-    for var in data:
-        nr = data[var].isna().sum()
-        if nr > 0:
-            mv[var] = nr
+# -------------- #
+# Missing values #
+# -------------- #
+mv = {}
+for var in data:
+    nr = data[var].isna().sum()
+    if nr > 0:
+        mv[var] = nr
 
-    figure()
-    bar_chart(list(mv.keys()), list(mv.values()), title='Nr of missing values per variable',
-                xlabel='variables', ylabel='nr missing values', rotation=True)
-    savefig('../images/mv.png')
-    show()
-
-
-def get_variable_types(df: DataFrame) -> dict:
-    variable_types: dict = {
-        'Numeric': [],
-        'Binary': [],
-        'Date': [],
-        'Symbolic': []
-    }
-    for c in df.columns:
-        uniques = df[c].dropna(inplace=False).unique()
-        if len(uniques) == 2:
-            variable_types['Binary'].append(c)
-            df[c].astype('bool')
-        elif df[c].dtype == 'datetime64':
-            variable_types['Date'].append(c)
-        elif df[c].dtype == 'int':
-            variable_types['Numeric'].append(c)
-        elif df[c].dtype == 'float':
-            variable_types['Numeric'].append(c)
-        else:
-            df[c].astype('category')
-            variable_types['Symbolic'].append(c)
-
-    return variable_types
-
-if __name__ == "__main__":
-    main()
+figure()
+bar_chart(list(mv.keys()), list(mv.values()), title='Nr of missing values per variable',
+            xlabel='variables', ylabel='nr missing values', rotation=True)
+savefig('./images/missing_values.png')
+# show()
---
+++ health_domain/data_profiling/data_granularity.py
@@ -0,0 +1,72 @@
+from pandas import read_csv
+from pandas.plotting import register_matplotlib_converters
+from ds_charts import get_variable_types, HEIGHT
+from matplotlib.pyplot import subplots, savefig, show
+
+register_matplotlib_converters()
+filename = '../datasets/classification/diabetic_data.csv'
+data = read_csv(filename, na_values='?')
+
+# print(data.shape)
+
+# -------------------------------- #
+# Histograms for numeric variables #
+# -------------------------------- #
+variables = get_variable_types(data)['Numeric']
+if [] == variables:
+    raise ValueError('There are no numeric variables.')
+
+rows = len(variables)
+bins = (5, 10, 50)
+cols = len(bins)
+fig, axs = subplots(rows, cols, figsize=(cols*HEIGHT, rows*HEIGHT), squeeze=False)
+for i in range(rows):
+    for j in range(cols):
+        axs[i, j].set_title('Histogram for %s %d bins'%(variables[i], bins[j]))
+        axs[i, j].set_xlabel(variables[i])
+        axs[i, j].set_ylabel('Nr records')
+        axs[i, j].hist(data[variables[i]].values, bins=bins[j])
+savefig('./images/granularity_study_numeric.png')
+# show()
+
+# --------------------------------- #
+# Histograms for symbolic variables #
+# --------------------------------- #
+
+variables = get_variable_types(data)['Symbolic']
+if [] == variables:
+    raise ValueError('There are no symbolic variables.')
+
+rows = len(variables)
+bins = (5, 10, 50)
+cols = len(bins)
+fig, axs = subplots(rows, cols, figsize=(cols*HEIGHT, rows*HEIGHT), squeeze=False)
+for i in range(rows):
+    for j in range(cols):
+        axs[i, j].set_title('Histogram for %s %d bins'%(variables[i], bins[j]))
+        axs[i, j].set_xlabel(variables[i])
+        axs[i, j].set_ylabel('Nr records')
+        axs[i, j].hist(data[variables[i]].values, bins=bins[j])
+savefig('./images/granularity_study_symbolic.png')
+# show()
+
+# ----------------------------- #
+# Histograms for date variables #
+# ----------------------------- #
+
+variables = get_variable_types(data)['Date']
+if [] == variables:
+    raise ValueError('There are no date variables.')
+
+rows = len(variables)
+bins = (5, 10, 50)
+cols = len(bins)
+fig, axs = subplots(rows, cols, figsize=(cols*HEIGHT, rows*HEIGHT), squeeze=False)
+for i in range(rows):
+    for j in range(cols):
+        axs[i, j].set_title('Histogram for %s %d bins'%(variables[i], bins[j]))
+        axs[i, j].set_xlabel(variables[i])
+        axs[i, j].set_ylabel('Nr records')
+        axs[i, j].hist(data[variables[i]].values, bins=bins[j])
+savefig('./images/granularity_study_date.png')
+# show()
