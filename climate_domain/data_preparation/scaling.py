96b1383d9dab43be19f1166d3fb9d6f80a7595ad
filipacotrim <filipa.cotrim@tecnico.ulisboa.pt>
2022-12-05 12:09:57 +0000

    without first colunn

--- climate_domain/data_preparation/scaling.py
+++ climate_domain/data_preparation/scaling.py
@@ -12,8 +12,8 @@ register_matplotlib_converters()
 file_name = 'drought'
 file_path = 'data/variables_encoding/drought_variables_encoding.csv'
 data = read_csv(file_path, na_values="na", sep=',', decimal='.', parse_dates=True, infer_datetime_format=True)
-# data['date'] = to_datetime(data['date'])
-
+first_column = data.columns[0]
+data = data.drop([first_column], axis = 1)
 variable_types = get_variable_types(data)
 print(variable_types)
 
