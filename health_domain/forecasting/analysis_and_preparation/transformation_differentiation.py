c2d4ef7872601fe1bd618fbba1824e81d2cd93f1
leonormbarreiros <leonormbarreiros@gmail.com>
2023-01-05 12:48:27 +0000

    persistence model

---
+++ health_domain/forecasting/analysis_and_preparation/transformation_differentiation.py
@@ -0,0 +1,45 @@
+from pandas import read_csv, Series
+from matplotlib.pyplot import figure, xticks, show, savefig, tight_layout
+from ts_functions import plot_series, HEIGHT
+
+file_tag = 'glucose'
+file_name = f'{file_tag}_100_smoothing'
+file_path = f'data/smoothing/{file_name}.csv'
+
+target = 'Glucose'
+index = 'Date'
+
+data = read_csv(file_path, index_col=index, sep=',', decimal='.', parse_dates=True, infer_datetime_format=True, dayfirst=True)
+
+# remove non-target columns for transformation
+# for column in data:
+#     if column != target:
+#         data.drop(columns=column, inplace=True)
+# print(data.shape)
+
+# sort data by date
+data.sort_values(by=data.index.name, inplace=True)
+
+# --------------- #
+# Differentiation #
+# --------------- #
+
+# first derivative
+diff_df = data.diff()
+diff_df.to_csv(f'data/differentiation/{file_tag}_1_differentiation.csv', index=True)
+
+figure(figsize=(3*HEIGHT, HEIGHT))
+plot_series(diff_df[target], title='Glucose - Differentiation (1st derivative)', x_label=index, y_label='measurement')
+plot_series(diff_df['Insulin'], x_label=index, y_label='measurement')
+xticks(rotation = 45)
+savefig(f'images/transformation/differentiation_1.png')
+
+# second derivative
+diff_df = diff_df.diff()
+diff_df.to_csv(f'data/differentiation/{file_tag}_2_differentiation.csv', index=True)
+
+figure(figsize=(3*HEIGHT, HEIGHT))
+plot_series(diff_df[target], title='Glucose - Differentiation (2nd derivative)', x_label=index, y_label='measurement')
+plot_series(diff_df['Insulin'], x_label=index, y_label='measurement')
+xticks(rotation = 45)
+savefig(f'images/transformation/differentiation_2.png')
---
+++ health_domain/forecasting/analysis_and_preparation/transformation_smoothing.py
@@ -0,0 +1,49 @@
+from pandas import read_csv, Series
+from matplotlib.pyplot import figure, xticks, show, savefig, tight_layout
+from ts_functions import plot_series, HEIGHT
+
+file_tag = 'glucose'
+file_name = f'{file_tag}_daily_aggregation'
+file_path = f'data/aggregation/{file_name}.csv'
+
+target = 'Glucose'
+index = 'Date'
+
+data = read_csv(file_path, index_col=index, sep=',', decimal='.', parse_dates=True, infer_datetime_format=True, dayfirst=True)
+
+# remove non-target columns for transformation
+# for column in data:
+#     if column != target:
+#         data.drop(columns=column, inplace=True)
+# print(data.shape)
+
+# sort data by date
+data.sort_values(by=data.index.name, inplace=True)
+
+# --------- #
+# Smoothing #
+# --------- #
+
+# first window size
+WIN_SIZE = 10
+rolling = data.rolling(window=WIN_SIZE)
+smooth_df = rolling.mean()
+smooth_df.to_csv(f'data/smoothing/{file_tag}_10_smoothing.csv', index=True)
+
+figure(figsize=(3*HEIGHT, HEIGHT/2))
+plot_series(smooth_df[target], title=f'Glucose - Smoothing (win_size={WIN_SIZE})', x_label=index, y_label='measurement')
+plot_series(smooth_df['Insulin'], x_label=index, y_label='measurement')
+xticks(rotation = 45)
+savefig(f'images/transformation/smoothing_10.png')
+
+# second window size
+WIN_SIZE = 100
+rolling = data.rolling(window=WIN_SIZE)
+smooth_df = rolling.mean()
+smooth_df.to_csv(f'data/smoothing/{file_tag}_100_smoothing.csv', index=True)
+
+figure(figsize=(3*HEIGHT, HEIGHT/2))
+plot_series(smooth_df[target], title=f'Glucose - Smoothing (win_size={WIN_SIZE})', x_label=index, y_label='measurement')
+plot_series(smooth_df['Insulin'], x_label=index, y_label='measurement')
+xticks(rotation = 45)
+savefig(f'images/transformation/smoothing_100.png')
