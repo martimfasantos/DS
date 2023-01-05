6f71582b0b022ae46f69ebe4f4ff1a506109976e
leonormbarreiros <leonormbarreiros@gmail.com>
2023-01-05 11:32:33 +0000

    transformation completing

--- health_domain/forecasting/analysis_and_preparation/transformation.py
+++ health_domain/forecasting/analysis_and_preparation/transformation.py
@@ -45,6 +45,8 @@ def aggregate_by(data: Series, index_var: str, period: str):
 # first aggregation
 figure(figsize=(3*HEIGHT, HEIGHT))
 agg_df = aggregate_by(data, index, 'D')
+agg_df.to_csv(f'data/aggregation/{file_tag}_daily_aggregation.csv', index=True)
+
 plot_series(agg_df[target], title='Glucose - Daily measurements', x_label=index, y_label='measurement')
 plot_series(agg_df['Insulin'], x_label=index, y_label='measurement')
 xticks(rotation = 45)
@@ -53,18 +55,22 @@ savefig(f'images/transformation/aggregation_daily.png')
 # second aggregation
 figure(figsize=(3*HEIGHT, HEIGHT))
 agg_df = aggregate_by(data, index, 'W')
+agg_df.to_csv(f'data/aggregation/{file_tag}_weekly_aggregation.csv', index=True)
+
 plot_series(agg_df[target], title='Glucose - Weekly measurements', x_label=index, y_label='measurement')
 plot_series(agg_df['Insulin'], x_label=index, y_label='measurement')
 xticks(rotation = 45)
 savefig(f'images/transformation/aggregation_weekly.png')
 
-# third aggregation
-figure(figsize=(3*HEIGHT, HEIGHT))
-agg_df = aggregate_by(data, index, 'M')
-plot_series(agg_df[target], title='Glucose - Monthly measurements', x_label=index, y_label='measurement')
-plot_series(agg_df['Insulin'], x_label=index, y_label='measurement')
-xticks(rotation = 45)
-savefig(f'images/transformation/aggregation_monthly.png')
+# # third aggregation
+# figure(figsize=(3*HEIGHT, HEIGHT))
+# agg_df = aggregate_by(data, index, 'M')
+# agg_df.to_csv(f'data/aggregation/{file_tag}_monthly_aggregation.csv', index=True)
+
+# plot_series(agg_df[target], title='Glucose - Monthly measurements', x_label=index, y_label='measurement')
+# plot_series(agg_df['Insulin'], x_label=index, y_label='measurement')
+# xticks(rotation = 45)
+# savefig(f'images/transformation/aggregation_monthly.png')
 
 # --------- #
 # Smoothing #
@@ -74,6 +80,8 @@ savefig(f'images/transformation/aggregation_monthly.png')
 WIN_SIZE = 10
 rolling = data.rolling(window=WIN_SIZE)
 smooth_df = rolling.mean()
+smooth_df.to_csv(f'data/smoothing/{file_tag}_10_smoothing.csv', index=True)
+
 figure(figsize=(3*HEIGHT, HEIGHT/2))
 plot_series(smooth_df[target], title=f'Glucose - Smoothing (win_size={WIN_SIZE})', x_label=index, y_label='measurement')
 plot_series(smooth_df['Insulin'], x_label=index, y_label='measurement')
@@ -84,6 +92,8 @@ savefig(f'images/transformation/smoothing_10.png')
 WIN_SIZE = 100
 rolling = data.rolling(window=WIN_SIZE)
 smooth_df = rolling.mean()
+smooth_df.to_csv(f'data/smoothing/{file_tag}_100_smoothing.csv', index=True)
+
 figure(figsize=(3*HEIGHT, HEIGHT/2))
 plot_series(smooth_df[target], title=f'Glucose - Smoothing (win_size={WIN_SIZE})', x_label=index, y_label='measurement')
 plot_series(smooth_df['Insulin'], x_label=index, y_label='measurement')
@@ -94,9 +104,22 @@ savefig(f'images/transformation/smoothing_100.png')
 # Differentiation #
 # --------------- #
 
+# first derivative
 diff_df = data.diff()
+diff_df.to_csv(f'data/differentiation/{file_tag}_1_differentiation.csv', index=True)
+
 figure(figsize=(3*HEIGHT, HEIGHT))
-plot_series(diff_df[target], title='Glucose - Differentiation', x_label=index, y_label='measurement')
+plot_series(diff_df[target], title='Glucose - Differentiation (1st derivative)', x_label=index, y_label='measurement')
 plot_series(diff_df['Insulin'], x_label=index, y_label='measurement')
 xticks(rotation = 45)
-savefig(f'images/transformation/differentiation.png')
+savefig(f'images/transformation/differentiation_1.png')
+
+# second derivative
+diff_df = diff_df.diff()
+diff_df.to_csv(f'data/differentiation/{file_tag}_2_differentiation.csv', index=True)
+
+figure(figsize=(3*HEIGHT, HEIGHT))
+plot_series(diff_df[target], title='Glucose - Differentiation (2nd derivative)', x_label=index, y_label='measurement')
+plot_series(diff_df['Insulin'], x_label=index, y_label='measurement')
+xticks(rotation = 45)
+savefig(f'images/transformation/differentiation_2.png')
