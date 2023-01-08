58ee6658ae010b136175c98309fd821c7645729f
leonormbarreiros <leonormbarreiros@gmail.com>
2023-01-08 22:25:01 +0000

    eval 7 leonor

---
+++ health_domain/forecasting/modeling_and_evaluation/simple_average.py
@@ -0,0 +1,70 @@
+from pandas import read_csv, DataFrame
+from matplotlib.pyplot import figure, subplots
+from ts_functions import HEIGHT, split_dataframe
+
+file_tag = 'glucose'
+file_name = f'{file_tag}'
+file_path = f'../datasets/{file_name}.csv'
+
+target = 'Glucose'
+index_col = 'Date'
+
+data = read_csv(file_path, index_col=index_col, sep=',', decimal='.', parse_dates=True, infer_datetime_format=True, dayfirst=True)
+
+# remove non-target columns
+for column in data:
+    if column != target:
+        data.drop(columns=column, inplace=True)
+        
+print(data.head())
+
+def split_dataframe(data, trn_pct=0.70):
+    trn_size = int(len(data) * trn_pct)
+    df_cp = data.copy()
+    train: DataFrame = df_cp.iloc[:trn_size, :]
+    test: DataFrame = df_cp.iloc[trn_size:]
+    return train, test
+
+train, test = split_dataframe(data, trn_pct=0.75)
+
+def plot_forecasting_series(trn, tst, prd_trn, prd_tst, figname: str, x_label: str = 'time', y_label:str =''):
+    _, ax = subplots(1,1,figsize=(5*HEIGHT, HEIGHT), squeeze=True)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.set_title(figname)
+    ax.plot(trn.index, trn, label='train', color='b')
+    ax.plot(trn.index, prd_trn, '--y', label='train prediction')
+    ax.plot(tst.index, tst, label='test', color='g')
+    ax.plot(tst.index, prd_tst, '--r', label='test prediction')
+    ax.legend(prop={'size': 5})
+
+
+measure = 'R2'
+flag_pct = False
+eval_results = {}
+
+from sklearn.base import RegressorMixin
+from ts_functions import PREDICTION_MEASURES, plot_evaluation_results, plot_forecasting_series
+
+class SimpleAvgRegressor (RegressorMixin):
+    def __init__(self):
+        super().__init__()
+        self.mean = 0
+
+    def fit(self, X: DataFrame):
+        self.mean = X.mean()
+
+    def predict(self, X: DataFrame):
+        prd =  len(X) * [self.mean]
+        return prd
+
+fr_mod = SimpleAvgRegressor()
+fr_mod.fit(train)
+prd_trn = fr_mod.predict(train)
+prd_tst = fr_mod.predict(test)
+
+eval_results['SimpleAvg'] = PREDICTION_MEASURES[measure](test.values, prd_tst)
+print(eval_results)
+
+plot_evaluation_results(train.values, prd_trn, test.values, prd_tst, f'images/simple_average/{file_tag}_simpleAvg_eval.png')
+plot_forecasting_series(train, test, prd_trn, prd_tst, f'{file_name} Simple Average Plots', saveto=f'images/simple_average/{file_tag}_simpleAvg_plots.png', x_label=index_col, y_label=target)
